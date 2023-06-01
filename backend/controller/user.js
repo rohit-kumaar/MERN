ce066dbb249d0b3fc17057cffc30d5b620ad6986
Rohit Kumar <rohit96682@gmail.com>
2023-06-01 22:09:50 +0530

    schema validation

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,6 +1,5 @@
-const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const DATA_COLLECTIONS = data.users;
+// const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+// const DATA_COLLECTIONS = data.users;
 
 const CREATE = (req, res) => {
   console.log(req.body);
--- backend/model/product.js
+++ backend/model/product.js
@@ -2,15 +2,23 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
 const productSchema = new Schema({
-  title: { type: String },
+  title: { type: String, require: true, unique: true },
   description: { type: String },
-  price: { type: Number },
-  discountPercentage: { type: Number },
-  rating: { type: Number },
+  price: { type: Number, min: [0, "Wrong Price"], required: true },
+  discountPercentage: {
+    type: Number,
+    min: [0, "Wrong min discount"],
+    max: [50, "Wrong max discount"],
+  },
+  rating: {
+    type: Number,
+    min: [0, "Wrong min rating"],
+    max: [5, "Wrong max rating"],
+  },
   stock: { type: Number },
-  brand: { type: String },
-  category: { type: String },
-  thumbnail: { type: String },
+  brand: { type: String, required: true },
+  category: { type: String, required: true },
+  thumbnail: { type: String, required: true },
   images: [String],
 });
 
